85b0a505378e7cf47940af049bb2d5a3a17ab2c3
yuio4235 <yuio4235@gmail.com>
2012-06-20 12:41:07 +0800

    commit DataInitialUtils.java ListViewUtils.java

--- src/com/as/ui/utils/DataInitialUtils.java
+++ src/com/as/ui/utils/DataInitialUtils.java
@@ -1,6 +1,7 @@
 package com.as.ui.utils;
 
 import com.as.db.provider.AsProvider;
+import com.as.db.provider.AsContent.SaSizeSet;
 
 import android.content.Context;
 import android.database.SQLException;
@@ -31,5 +32,31 @@ public class DataInitialUtils {
 				db.close();
 			}
 		}
+	}
+	
+	public static void InitSaSizeSet(Context context) {
+		SaSizeSet sasizeset = new SaSizeSet();
+		sasizeset.sizeGroup = "11";
+		sasizeset.s01 = 10;
+		sasizeset.s02 = 10;
+		sasizeset.s03 = 10;
+		sasizeset.s04 = 10;
+		sasizeset.s05 = 10;
+		sasizeset.s06 = 10;
+		sasizeset.s07 = 10;
+		sasizeset.s08 = 10;
+		sasizeset.s09 = 10;
+		sasizeset.s10 = 10;
+		sasizeset.s11 = 10;
+		sasizeset.s12 = 10;
+		sasizeset.s13 = 10;
+		sasizeset.s14 = 10;
+		sasizeset.s15 = 10;
+		sasizeset.s16 = 10;
+		sasizeset.s17 = 10;
+		sasizeset.s18 = 10;
+		sasizeset.s19 = 10;
+		sasizeset.s20 = 10;
+		context.getContentResolver().insert(SaSizeSet.CONTENT_URI, sasizeset.toContentValues());
 	}
 }
